112f02d304b9ef68732225c82f3f39a5b8c3992b
prasoona <prasoona@caratred.com>
2019-11-25 06:47:47 +0530

    added buttons in tool bar

--- uitask/src/app/nav/nav.component.ts
+++ uitask/src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -8,10 +8,10 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
   notes: string;
   todayDate = new Date();
-
+  noteTitles: Array<{}>;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
@@ -19,6 +19,15 @@ export class NavComponent {
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(){
+    this.noteTitles = [
+      {id:4, title: 'Note1',  date: new Date() },
+      {id:3, title: 'Note2',  date: new Date() },
+      {id:2, title: 'Note3',  date: new Date() },
+      {id:1, title: 'Note4',  date: new Date() }
+    ]
+  }
   receiveNote(event: any) {
     console.log(event, 'event',)
     this.notes = event;
